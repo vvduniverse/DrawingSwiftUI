ce35bb7b9e7a93a29360815e176620116b437119
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-16 18:50:24 +0200

    Initial Commit

---
+++ DrawingSwiftUI/DrawingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingSwiftUIApp.swift
+//  DrawingSwiftUI
+//
+//  Created by Vahtee Boo on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
